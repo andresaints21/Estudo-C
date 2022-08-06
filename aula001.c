806b4ce35c4cbe4cba3cc8b321bd13f4ed83e679
André Santos <andrelimasantos21@gmail.com>
2022-08-06 06:04:59 -0300

    novo

--- aula001.c
+++ aula001.c
@@ -1,9 +1,23 @@
 int main(int argc, char const *argv[])
 {
-    int valor;
-    printf("Digite um numero: ");
-    scanf("%d",&valor);
-    printf("\n o numero é %d",valor);
+    char nome [30];
+    char endereco[30];
+    int idade;
+
+    printf("Nome \n");
+    scanf("%s", &nome);
+
+    printf("Endereço \n");
+    scanf("%s", &endereco);
+
+    printf("Idade \n");
+    scanf("%d", &idade);
+
+    printf("\n Nome: %s", nome);
+    printf("\n Endereço: %s", endereco);
+    printf("\n Idade: %d", idade);
+
     return 0;
+
 }
 
